642909920d660d0c78f3b03211a5ba3ee2281404
Evan <evandp1999@gmail.com>
2020-06-13 01:22:51 -0400

    Updates

---
+++ precache-manifest.d837a2b672cf6c7f914725c3ac39bfa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8529d20a320309045a4dff2610089c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b7e42ec7bbd67351a6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e9e8ed3f118caa28d658",
+    "url": "/static/js/2.77369cd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77369cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82b7e42ec7bbd67351a6",
+    "url": "/static/js/main.186222b5.chunk.js"
+  },
+  {
+    "revision": "cd6813dcbf5a9c5cbccb",
+    "url": "/static/js/runtime-main.2594d068.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
